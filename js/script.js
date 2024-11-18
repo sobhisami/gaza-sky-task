d2055795d3ce3f8743ebbbe32fd4d72230f85c1f
sobhi <sobhi@alMacBook-Air-alkhas-bsobhi.local>
2024-11-18 13:15:55 +0200

    task 2 sum array and ave

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-window.alert('hi')
+let num = [1,2,3,4,5,6,7,8,9,10]
+
+let sum= num.reduce((p,c)=>{
+    return p+c;
+})
+let average = sum/num.length;
+console.log("sum the arr: "+sum);
+console.log("average the arr: "+average);
