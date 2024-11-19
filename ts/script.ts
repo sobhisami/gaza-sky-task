a0e152a7ba18947172e27228ddaf74cb3ae7ec81
sobhi <sobhi@alMacBook-Air-alkhas-bsobhi.local>
2024-11-19 09:55:44 +0200

    make function cheak email

--- ts/script.ts
+++ ts/script.ts
@@ -1,3 +1,4 @@
+//ex TypeScript function total price
 interface Product {
     name: string;
     price: number;
@@ -14,3 +15,15 @@ const products: Product[] = [
 
 const totalPrice = calculateTotalPrice(products);
 console.log(`Total Price: $${totalPrice}`);
+
+// ex five
+function isValidEmail(email: string): boolean {
+    const emails = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emails.test(email);
+}
+
+const email1 = "sobhi@gmail.com";
+const email2 = "invalid-email";
+
+console.log(`${email1} is valid: ${isValidEmail(email1)}`);
+console.log(`${email2} is valid: ${isValidEmail(email2)}`);
