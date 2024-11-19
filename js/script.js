4ac3f67d6730409fb0ef7e5f6d62eb229f56b61f
sobhi <sobhi@alMacBook-Air-alkhas-bsobhi.local>
2024-11-19 09:46:03 +0200

    unique string

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// ex sum array and average
 let num = [1,2,3,4,5,6,7,8,9,10]
 
 let sum= num.reduce((p,c)=>{
@@ -6,3 +7,8 @@ let sum= num.reduce((p,c)=>{
 let average = sum/num.length;
 console.log("sum the arr: "+sum);
 console.log("average the arr: "+average);
+
+//ex unique values
+let name =["sobhi","sami","rami","sobhi","sobhi","sami"]
+let unName=[...new Set(name)]
+console.log(unName);
